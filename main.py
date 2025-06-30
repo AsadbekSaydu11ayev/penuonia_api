e4f2d10a2d008c9a766ad64c0802be3d919dbb9c
AsadbekSaydu11yev <saydullayevasadbek16@gmail.com>
2025-06-30 19:57:39 +0500

    initial commit

--- main.py
+++ main.py
@@ -1,33 +1,19 @@
 from fastapi import FastAPI, File, UploadFile
 from fastai.vision.all import *
-import pathlib
-pathlib.PosixPath = pathlib.WindowsPath
-# rasmlar bilan ishlash uchun
-# working with images
-from PIL import Image
-# for reading images as bytes
-# tasvirlarni bayt oqimi sifatida o'qish uchun
-import io
+from io import BytesIO
+import pathlib, platform
+plt = platform.system()
+if plt == "Linux": pathlib.WindowsPath = pathlib.PosixPath
 
-# FastAPI ilovasini yaratamiz
-# we will create FastAPI application
-app = FastAPI() # you can lounch API via this app
-
-# we upload the fastapi model
-learn = load_learner("pnevmaniya_model.pkl")
-
+app = FastAPI()
+learn = load_learner("export.pkl")
 
 @app.post("/predict")
-async def predict_img(file: UploadFile = File(...)):
-
+async def predict_image(file: UploadFile = File(...)):
     contents = await file.read()
-    # file bytes holatida o'qish
-
-    img = PILImage.create(io.BytesIO(contents))
-
-    pred, pred_id, probs = learn.predict(img)
-
+    img = PILImage.create(BytesIO(contents))
+    pred_class, pred_idx, probs = learn.predict(img)
     return {
-        "prediction": str(pred), # Bashorat natijasini JSON formatda qaytaramiz
-        "probability": f"{probs[pred_id]}"
+        "prediction": str(pred_class),
+        "probability": f"{probs[pred_idx]:.4f}"
     }
